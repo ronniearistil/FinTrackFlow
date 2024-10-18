e6857f1b31efd297a8450c95334e31adbb8a5282
Ronnie Aristil <ronnie.aristil@gmail.com>
2024-10-18 15:57:01 -0700

    update NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { AppBar, Toolbar, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li><NavLink to="/projects" activeClassName="active">Projects</NavLink></li>
-        <li><NavLink to="/projects/new" activeClassName="active">Add Project</NavLink></li>
-        <li><NavLink to="/expenses" activeClassName="active">Expenses</NavLink></li>
-        <li><NavLink to="/expenses/new" activeClassName="active">Add Expense</NavLink></li>
-      </ul>
-    </nav>
+    <AppBar position="static" style={{ backgroundColor: '#040404d4' }}>
+      <Toolbar>
+        <Button component={Link} to="/projects" color="inherit">Projects</Button>
+        <Button component={Link} to="/projects/new" color="inherit">Add Project</Button>
+        <Button component={Link} to="/expenses" color="inherit">Expenses</Button>
+        <Button component={Link} to="/expenses/new" color="inherit">Add Expense</Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
