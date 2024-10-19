1731a6318e39669cb8231037472cf8a66c87ca9d
Ronnie Aristil <ronnie.aristil@gmail.com>
2024-10-18 22:04:21 -0700

    update README Detail

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -6,6 +6,8 @@ import DashBoard from './DashBoard.jsx';
 import ExpenseDashboard from './ExpenseDashboard.jsx';
 import ProjectForm from './ProjectForm.jsx';
 import ExpenseForm from './ExpenseForm.jsx';
+import ProjectDetail from './ProjectDetail';
+import ExpenseDetail from './ExpenseDetail';
 import Footer from './Footer.jsx';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
@@ -17,14 +19,13 @@ const App = () => {
         <Header />
         <NavBar />
         <Routes>
-          <Route path="/" element={<Navigate to="/projects" />} />
-          <Route path="/projects" element={<DashBoard />} />
-          <Route path="/projects/new" element={<ProjectForm />} />
-          <Route path="/projects/:id/edit" element={<ProjectForm />} />
-          <Route path="/expenses" element={<ExpenseDashboard />} />
-          <Route path="/expenses/new" element={<ExpenseForm />} />
-          <Route path="*" element={<h2>Page Not Found</h2>} />
-        </Routes>
+  <Route path="/" element={<Navigate to="/projects" />} />
+  <Route path="/projects" element={<DashBoard />} />
+  <Route path="/projects/new" element={<ProjectForm />} />
+  <Route path="/projects/:id" element={<ProjectDetail />} /> {/* Route for individual project */}
+  <Route path="/expenses" element={<ExpenseDashboard />} />
+  <Route path="/expenses/new" element={<ExpenseForm />} />
+</Routes>
         <Footer />
       </div>
     </ProjectProvider>
